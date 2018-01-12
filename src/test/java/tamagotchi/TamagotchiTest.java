920d9128bdcc6328fbe9a7c61c8a5ffb3960befd
Antonio de la Torre <antonio@codesai.com>
2018-01-12 23:54:01 +0100

    Bootstrap Tamagotchi Kata

---
+++ src/test/java/tamagotchi/TamagotchiTest.java
@@ -0,0 +1,14 @@
+package tamagotchi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class TamagotchiTest {
+
+    @Test
+    void test_works() {
+        assertThat(true, is(false));
+    }
+}
